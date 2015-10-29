83ca4e2576c52acc3d415a6ceeea43a907c5699a
Mariya Gnitetskaya <tennalian@gmail.com>
2015-10-29 18:07:50 +0200

    edit

--- js/script.js
+++ js/script.js
@@ -15,7 +15,6 @@ function init(){
     ev = 'right';
 
     snikers();
-    // paintSnake();
     food();
 
     var requestAnimationFrame = window.mozRequestAnimationFrame||
@@ -23,6 +22,9 @@ function init(){
                                 window.webkitRequestAnimationFrame;
 
     requestAnimationFrame(loop);
+
+
+
 
 
 };
@@ -44,25 +46,37 @@ function loop() {
         var key = e.which;
         ev = keys[key];
     }, false);
-// console.log(ev);
-    if(ev == 'right') nx++;
-    else if(ev == 'left') nx--; // столбец
-    else if(ev == 'up') ny--;// строка
-    else if(ev == 'down') ny++;
 
-    //ctx.clearRect(snake_array[0].x,snake_array[0].y,10,10);
-    ctx.fillRect(snake_array[snake_array.length-1].x,snake_array[snake_array.length-1].y,10,10);
+    if(ev == 'right') nx+=10;
+    else if(ev == 'left') nx=nx-10;
+    else if(ev == 'up') ny=ny-10;
+    else if(ev == 'down') ny+=10;
 
-    //Move snake
+
+    ctx.fillRect(snake_array[0].x,snake_array[0].y,count,count);
+    ctx.clearRect(snake_array[snake_array.length-1].x,snake_array[snake_array.length-1].y,count,count);
+
+
+
+    // //Move snake
     var tail = snake_array.pop();
     tail.x = nx;
     tail.y = ny;
     snake_array.unshift(tail);
 
 
+    if ((food.x == snake_array[0].x)&&(food.y == snake_array[0].y)){
+        console.log('omnomnom');
+        snake_array.push(food.x,food.y);
+    }
+
+    if ((snake_array[0].x == width) || (snake_array[0].y == height)){
+        console.log('game over');
+    }
+
+    setTimeout(loop, 200);
 
 
-    requestAnimationFrame(loop);
 }
 
 function draw(x,y){
@@ -71,25 +85,27 @@ function draw(x,y){
 
 
 function food(){
-    ctx.fillStyle = 'green'; //apple green
-    var x = Math.floor(Math.random()*(width - count));
-    var y = Math.floor(Math.random()*(height - count));
-    ctx.fillRect(x, y, 10, 10);
+    ctx.fillStyle = 'green';
+    var food={
+        x : Math.floor(Math.random()*(width - count)),
+        y : Math.floor(Math.random()*(height - count))
+    };
+    ctx.fillRect(food.x, food.y, 10, 10);
 };
 
 function snikers(){
-    ctx.fillStyle = 'blue';
     for (var i=0; i<length; i++){
         snake_array.push({x:i,y:0});
         draw(snake_array[i].x,snake_array[i].y);
     }
 };
 
-function paintSnake() {
+function update() {
     for(var i = 0; i < snake_array.length; i++) {
         ctx.clearRect(snake_array[i].x,snake_array[i].y,10,10);
     }
     for(var i = 0; i < snake_array.length; i++) {
+
         var s = snake_array[i];
         draw(s.x, s.y);
     }
